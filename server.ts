889028047e408adb5e5f0d0d01fb6d130b35e779
Durgesh Nandan Mohanty <durgeshnandanmohanty@gmail.com>
2026-07-15 10:40:43 +0530

    fix: bind host to 0.0.0.0 for Render

--- server.ts
+++ server.ts
@@ -67,6 +67,6 @@ app.get('/', (req: Request, res: Response): void => {
 
 const PORT: number | string = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT as number, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
